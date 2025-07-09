e0b56adba52e2a377ae970a525392c4a58aa17b9
Ajsa <besirovicajsa@gmail.com>
2025-07-09 20:50:18 +0200

    Setup login with BetterAuth and Prisma

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
+import { emailOTP } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        // need to implement sending mail with otp
+      },
+    }),
+  ],
+});
