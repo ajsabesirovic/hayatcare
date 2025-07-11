2d1dcd1679a98ec95de0e0ec2084da1ee9229fc4
edinamaljevac <maljevacedina@gmail.com>
2025-07-11 14:04:55 +0200

    theme toggle added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,30 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-    title: "Hayat Care",
-    description:
-        "Aplikacija za povezivanje volontera i starijih osoba kojima je potrebna pomoć u svakodnevnom životu.",
+  title: "Hayat Care",
+  description:
+    "Aplikacija za povezivanje volontera i starijih osoba kojima je potrebna pomoć u svakodnevnom životu.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
