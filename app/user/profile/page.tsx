3666359705231b7194a0d23a516e28a0d7ecb432
edinamaljevac <maljevacedina@gmail.com>
2025-07-21 12:23:19 +0200

    site-header changed

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -3,5 +3,10 @@ import { getCurrentDbUser } from "@/dal/user";
 
 export default async function ProfilePage() {
   const user = await getCurrentDbUser();
+
+  if (!user) {
+    return <div>Molimo ulogujte se da biste pristupili profilu.</div>;
+  }
+
   return <Profile user={user} />;
 }
